51e9812e9b05a5d57ccfd12e843456cbb2e83a4c
Martin Hamel <martin@komunide.com>
2021-03-09 11:31:28 -0500

    fixed missing client

--- backend/src/chat.js
+++ backend/src/chat.js
@@ -2,16 +2,18 @@ const twilio = require("twilio");
 
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
+const client = twilio(accountSid, authToken);
 
  
 module.exports = {
   chat: async (socket, io) => {
     socket.on("action", async (action) => {
+      console.log(action);
       switch(action.type) {
         case 'joined':
           socket.join(action.roomId);
           const token = await client.tokens.create();
-          socket.emit({
+          socket.emit('action', {
             type: 'iceTokenCreated',
             token
           })
